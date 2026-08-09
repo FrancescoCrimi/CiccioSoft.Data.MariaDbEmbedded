b135c09e1c97821d18dcd7a3b8fa1acb6f643e4a
Francesco Crimi <francrim@hotmail.com>
2026-08-09 23:38:45 +0200

    fix licenze

--- CiccioSoft.Interop.MariaDb/README.md
+++ CiccioSoft.Interop.MariaDb/README.md
@@ -25,3 +25,20 @@ client.Ping();
 
 - This is an initial scaffold and intentionally minimal.
 - Runtime requires a compatible `libmysqlclient` (or equivalent MariaDB client library) on the host system.
+
+## License
+CiccioSoft.Mariadb: A lightweight C# class library for high-performance MariaDB database access.
+Copyright (C) 2026  Francesco Crimi
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 2 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
--- CiccioSoft.Interop.MariaDb/SafeHandles.cs
+++ CiccioSoft.Interop.MariaDb/SafeHandles.cs
@@ -1,8 +1,18 @@
-// Copyright (c) 2026 Francesco Crimi
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+// CiccioSoft.Mariadb: A lightweight C# class library for high-performance MariaDB database access.
+// Copyright (C) 2026  Francesco Crimi
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 using System.Runtime.InteropServices;
 using CiccioSoft.Interop.MariaDb.Native;
