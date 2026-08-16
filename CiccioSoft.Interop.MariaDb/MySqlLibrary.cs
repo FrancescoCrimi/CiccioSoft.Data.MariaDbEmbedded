09d060e84c9b7e2bcb94a130e21fb37bd9605bb5
Francesco Crimi <francrim@hotmail.com>
2026-08-16 16:36:42 +0200

    fix license LGPLv2.1

--- CiccioSoft.Interop.MariaDb/MySqlLibrary.cs
+++ CiccioSoft.Interop.MariaDb/MySqlLibrary.cs
@@ -1,18 +1,18 @@
 // CiccioSoft.Mariadb: A lightweight C# class library for high-performance MariaDB database access.
-// Copyright (C) 2026  Francesco Crimi
-// 
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; version 2 of the License.
-// 
-// This program is distributed in the hope that it will be useful,
+// Copyright (C) 2026  Francesco Crimi.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, see https://www.gnu.org/licenses/.
 
 using System;
 using System.Threading;
--- README.md
+++ README.md
@@ -55,18 +55,7 @@ The primary goal is to provide a .NET provider for MariaDB Embedded composed of 
 - Use `AGENTS.md` in individual project folders for project-specific rules and build/test commands.
 
 ## License
-CiccioSoft.Mariadb: A lightweight C# class library for high-performance MariaDB database access.
-Copyright (C) 2026  Francesco Crimi
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 of the License.
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+See the [LICENSE](LICENSE) file for the full license text.
